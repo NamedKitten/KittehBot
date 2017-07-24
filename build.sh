a0ac975ecf70ab7bc833e4c2cd62eb4a24cbeb33
NamedKitten <kitteh@namedkitten.cf>
2017-07-24 17:46:04 +0100

    Update?

--- build.sh
+++ build.sh
@@ -1,16 +1,22 @@
 #!/bin/bash
 echo "doing teh git"
-git submodule update --init --recursive
+git submodule update --init --recursive --remote --force
 echo "making teh build"
 mkdir build
 echo "going into build"
 cd build
-echo "downloading teh cmake"
-wget https://cmake.org/files/v3.9/cmake-3.9.0-Linux-x86_64.tar.gz
-echo "extracting teh cmake"
-tar xf cmake-3.9.0-Linux-x86_64.tar.gz
+if [ ! -f cmake-3.9.0-Linux-x86_64.tar.gz ]; then
+  echo "downloading teh cmake"
+  wget https://cmake.org/files/v3.9/cmake-3.9.0-Linux-x86_64.tar.gz
+  echo "extracting teh cmake"
+  tar xf cmake-3.9.0-Linux-x86_64.tar.gz
+fi
+if [ -f CMakeCache.txt ]; then
+    rm CMakeCache.txt
+fi
 echo "using teh cmake"
 cmake-3.9.0-Linux-x86_64/bin/cmake ..
 echo "using make"
+make clean
 make -j7
 echo "o.o it is done o.o"
