9bc621327b896a4c8c44bcbd0065544477b76baa
NamedKitten <kitteh@namedkitten.cf>
2017-07-29 02:45:10 +0100

    Update.

--- build.sh
+++ build.sh
@@ -10,23 +10,22 @@ echo "making teh build"
 mkdir build
 echo "going into build"
 cd build
-if [ ! -f cmake-3.9.0-Linux-x86_64.tar.gz ]; then
-  echo "downloading teh cmake"
-  wget https://cmake.org/files/v3.9/cmake-3.9.0-Linux-x86_64.tar.gz
-  echo "extracting teh cmake"
-  tar xf cmake-3.9.0-Linux-x86_64.tar.gz
-fi
+#if [ ! -f cmake-3.9.0-Linux-x86_64.tar.gz ]; then
+#  echo "downloading teh cmake"
+#  wget https://cmake.org/files/v3.9/cmake-3.9.0-Linux-x86_64.tar.gz
+#  echo "extracting teh cmake"
+#  tar xf cmake-3.9.0-Linux-x86_64.tar.gz
+#fi
 if [ -f CMakeCache.txt ]; then
     rm * -rf
 fi
+export CC=clang-5.0 CXX=clang++-5.0
 echo "using teh cmake"
-cmake-3.9.0-Linux-x86_64/bin/cmake ..
+cmake ..
 echo "using make"
 make clean
-
 if make -j`nproc` ; then
 	echo "o.o it is done o.o"
 else
 	exit 1
 fi
-cd ..
