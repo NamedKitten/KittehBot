c07aacc58b4135b50fa645c21ffdf50d55eea556
NamedKitten <kitteh@namedkitten.cf>
2017-08-15 15:36:36 +0100

    Made build script friendlyer. The build script was used for Jenkins only. However now that we don't have Jenkins we can make it for users instead.

--- build.sh
+++ build.sh
@@ -1,14 +1,10 @@
 #!/bin/bash
-echo `whoami`
-echo `nproc`
-ls -lah
-echo potato
+echo "Please make sure you have at least cmake version 3.8 or the build will fail."
 rm lib/* -rf
-echo "doing teh git"
+echo "Updating submodules..."
 git submodule update --init --recursive --force --remote --depth 1
-echo "making teh build"
-mkdir build
-echo "going into build"
+echo "Making build directory..."
+mkdir -v build
 cd build
 #if [ ! -f cmake-3.9.0-Linux-x86_64.tar.gz ]; then
 #  echo "downloading teh cmake"
@@ -17,19 +13,21 @@ cd build
 #  tar xf cmake-3.9.0-Linux-x86_64.tar.gz
 #fi
 if [ -f CMakeCache.txt ]; then
+    echo "Cleaning up..."
     rm * -rf
 fi
-export VERBOSE=1
-echo "using teh cmake"
+echo "Running cmake..."
 cmake .. -DTEST=yes
-echo "using make"
+echo "Cleaning build directory."
 make clean
-
-if make -j`nproc` VERBOSE=1 ; then
+echo "Making bot..."
+make -j`nproc` VERBOSE=1
+echo "Done! The executable will be in the build directory."
+#if make -j`nproc` VERBOSE=1 ; then
 #	redis-server &
 #	sleep 5
 #	./KittehBot++
-        sleep 1
-else
-	exit 1
-fi
+#        sleep 1
+#else
+#	exit 1
+#fi
